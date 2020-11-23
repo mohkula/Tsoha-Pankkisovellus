1c27d80229791433fa7d814a0ce2a2182abb3a40
Matias Himanen <matias.himanen@helsinki.fi>
2020-11-23 18:44:39 +0200

    lisätty mahdollisuus katsella omia korttitietojaan

--- userPage.py
+++ userPage.py
@@ -23,7 +23,22 @@ def userPage():
 
     return redirect("/")
     
-    
+
+@app.route("/showCards")
+def showCards():
+    if(session["username"]):
+		
+        username = session["username"]
+
+        sql = "SELECT card_number, expirationdate FROM cards WHERE customer_id= (SELECT id FROM users WHERE username =:username)"
+        result = db.session.execute(sql, {"username":username})
+        card_info = result.fetchall()
+	
+	
+        return render_template("userPage.html", cardInfo = card_info)
+
+    return redirect("/")
+
 @app.route("/addCard", methods =["POST"])    
 def addCard():
     if(session["username"]):
@@ -52,9 +67,11 @@ def addCard():
         db.session.execute(sql, {"card_number":card_number})
         db.session.commit()
         
-        
+        return render_template("userPage.html")
+    
+    return redirect("/")    
             
-    return render_template("userPage.html")
+    
 
     
 	
