4bc94fffc063b88d8731ec2a3696ca65da4425fa
Matias Himanen <matias.himanen@helsinki.fi>
2020-12-05 13:01:18 +0200

    varoitustaulu luotu

--- schema.sql
+++ schema.sql
@@ -8,4 +8,7 @@
 
 
 
-CREATE TABLE orders(id SERIAL PRIMARY KEY, type INTEGER , customer_id INTEGER NOT NULL DEFAULT 0, username TEXT, orderingDate DATE DEFAULT now())
+CREATE TABLE orders(id SERIAL PRIMARY KEY, type INTEGER , customer_id INTEGER NOT NULL DEFAULT 0, username TEXT, orderingDate DATE DEFAULT now());
+
+
+CREATE TABLE cardWarnings(id SERIAL PRIMARY KEY, card_id INTEGER NOT NULL, type INTEGER NOT NULL, date DATE DEFAULT now(),active BOOLEAN DEFAULT FALSE);
